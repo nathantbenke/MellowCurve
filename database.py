54ceb239383cf599464052cf7bbf9254a332d382
cverh <80092212+cverh@users.noreply.github.com>
2023-03-24 08:54:43 -0500

    Update database.py

--- database.py
+++ database.py
@@ -5,23 +5,6 @@
 
 import mysql.connector
 
-# this class represents a sample
-class Sample:
-    # constructor
-    # id:           unique integer sample id
-    # name:         string name that the sample is referred to as
-    # audio:        blob file
-    # sample_owner: unique integer user id of who uploaded the sample
-    def __init__(self, id, name, audio, sample_owner):
-        self.id = id
-        self.name = name
-        self.audio = audio
-        self.sample_owner = sample_owner
-        
-    # prints formatted sample
-    def __str__(self):
-        return "sample " + str(self.id) + ": '" + self.name + "' accessed by user " + str(self.sample_owner)
-
 # creates samples and user_permissions tables
 # removes old tables if they exist
 # cnx:  the database connection
@@ -52,7 +35,7 @@ def create_tables(cnx):
     cnx.commit()
     cursor.close()
 
-# inserts a sample into the database.
+# inserts a sample into the database
 # cnx:          the database connection
 # sample_id:    the id of the sample
 # sample_name:  the name of the sample
@@ -60,10 +43,10 @@ def create_tables(cnx):
 # sample_owner: user_id of who uploaded the sample
 def insert_sample(cnx, sample_id, sample_name, file_name, sample_owner):
     cursor = cnx.cursor()
-    
     # turns file_name into BLOB so it can be added to db
     with open(file_name, 'rb') as file:
         audio_blob = file.read()
+        
     # inserts sample into samples table
     samples_query = ("INSERT INTO samples "
             "(id, name, audio_blob, owner) "
@@ -97,6 +80,53 @@ def share_sample(cnx, user_id, sample_id):
     cnx.commit()
     cursor.close()
     
+# returns audio blob of a given sample id
+# cnx:          the database connection
+# sample_id:    the sample id to search for
+def get_sample_blob(cnx, sample_id):
+    cursor = cnx.cursor()
+    query = ("SELECT S.audio_blob FROM samples S "
+            "WHERE S.id = " + str(sample_id))
+    cursor.execute(query)
+    return cursor.fetchone()[0]
+    
+# returns owner of a given sample id
+# cnx:          the database connection
+# sample_id:    the sample id to search for
+def get_sample_owner(cnx, sample_id):
+    cursor = cnx.cursor()
+    query = ("SELECT S.owner FROM samples S "
+            "WHERE S.id = " + str(sample_id))
+    cursor.execute(query)
+    return cursor.fetchone()[0]
+    
+# returns name of a given sample id
+# cnx:          the database connection
+# sample_id:    the sample id to search for
+def get_sample_name(cnx, sample_id):
+    cursor = cnx.cursor()
+    query = ("SELECT S.name FROM samples S "
+            "WHERE S.id = " + str(sample_id))
+    cursor.execute(query)
+    return cursor.fetchone()[0]
+
+# returns list of all sample ids a user has access to
+# cnx:  the database connection
+# user: the user id to search for
+def get_user_samples(cnx, user):
+    samples = []
+    cursor = cnx.cursor()
+    
+    query = ("SELECT S.id FROM user_permissions U, samples S "
+            "WHERE U.user_id = " + user + " AND S.id = U.sample_id")
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for r in result:
+        samples.append(r[0])
+        
+    cursor.close()
+    return samples
+    
 # removes access to a sample from a user by
 # removing the record from user_permissions table
 # cnx:          the database connection
@@ -122,42 +152,24 @@ def remove_sample(cnx, sample_id):
     cursor.execute(query)
     cnx.commit()
     cursor.close()
-
-# returns list of all samples a user has access to
-# cnx:  the database connection
-# user: the user id to search for
-def get_user_samples(cnx, user):
-    samples = []
-    cursor = cnx.cursor()
-    
-    query = ("SELECT * FROM user_permissions U, samples S "
-            "WHERE U.user_id = " + user + " AND S.id = U.sample_id")
-    cursor.execute(query)
-    result = cursor.fetchall()
-    for r in result:
-        samples.append(Sample(id=r[2],name=r[3],audio=r[1],sample_owner=r[0]))
-        
-    cursor.close()
-    return samples
     
 # establish database connection
-# adjust user/password/host/database as necessary
 cnx = mysql.connector.connect(user="root", password="password", database="samples")
 # run queries
 create_tables(cnx)
 insert_sample(cnx, "1", "bird noises", "./birdnoises.mp3", "101")
-insert_sample(cnx, "2", "more bird noises", "./birdnoises.mp3", "102")
-insert_sample(cnx, "3", "noises", "./birdnoises.mp3", "102")
-insert_sample(cnx, "4", "birds?", "./birdnoises.mp3", "101")
+insert_sample(cnx, "2", "guitar", "./guitar.wav", "102")
+insert_sample(cnx, "3", "more bird noises", "./birdnoises.mp3", "102")
+insert_sample(cnx, "4", "more guitar", "./guitar.wav", "101")
 share_sample(cnx, "101", "2")
 hide_sample(cnx, "102", "3")
 remove_sample(cnx, "4")
 # check results
 samples_101 = get_user_samples(cnx, "101")
 samples_102 = get_user_samples(cnx, "102")
-for sample in samples_101:
-    print(sample)
-for sample in samples_102:
-    print(sample)
+for s in samples_101:
+    print("sample " + str(s) + ": '" + get_sample_name(cnx,s) + "' can be accessed by user 101.")
+for s in samples_102:
+    print("sample " + str(s) + ": '" + get_sample_name(cnx,s) + "' can be accessed by user 102.")
 # close connection
 cnx.close()
